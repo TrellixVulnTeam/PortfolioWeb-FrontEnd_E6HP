276834fa3aca9a00339436ccddca5c06c059c8c2
SAURION <pachy.sms@gmail.com>
2022-07-27 00:05:44 -0300

    COMMIT 26Jul22- TRABAJO EN FRONT

--- portfolioWeb/src/app/componentes/persona/persona.component.ts
+++ portfolioWeb/src/app/componentes/persona/persona.component.ts
@@ -7,14 +7,19 @@ import { PortfolioWebService } from 'src/app/servicios/portfolio-web.service';
   styleUrls: ['./persona.component.css']
 })
 export class PersonaComponent implements OnInit {
+  dataTitulos:any;
   dataPortfolio:any;
   constructor(private datosPortfolio : PortfolioWebService) { }
 
   ngOnInit(): void {
     this.datosPortfolio.obtenerDatos().subscribe(data => {
       this.dataPortfolio=data;
+      this.dataTitulos=data.titulo;
       console.log(this.dataPortfolio);
-    })    
+      console.log(this.dataTitulos);
+    })
+     
   }
 
+  
 }
